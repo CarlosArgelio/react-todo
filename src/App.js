a9c88dc89596339b5675ddeed9b0a57b0b93d0f3
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-08-03 00:13:59 -0400

    feat: structure componnects react

--- src/App.js
+++ src/App.js
@@ -5,24 +5,18 @@ function App() {
   return (
     <div className="App">
 
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      <TodoCounter />
+      <TodoSearch />
 
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita el archivo <code>src/App.js</code> y guarda para recargar.
-        </p>
-        <a
-          className="App-link"
-          href="https://platzi.com/reactjs"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoList>
+
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+
+      </TodoList>
+
+      <TodoBotom />
     </div>
   );
 }
