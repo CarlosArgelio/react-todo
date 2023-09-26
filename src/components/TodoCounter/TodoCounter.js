2597e29851637a5e8120f6988898c2d887f6cc63
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-09-26 17:28:27 -0400

    fix: change style TodoCounter

--- src/components/TodoCounter/TodoCounter.js
+++ src/components/TodoCounter/TodoCounter.js
@@ -1,11 +1,11 @@
-import styles from './TodoCounter.style'; 
+import './TodoCounter.css';
 
 function TodoCounter({ total, completed }) {
-    return (
-      <h1 style={styles.todoCounter}>
-        Has completado {completed} de {total}
-      </h1>
-    );
+  return (
+    <h1 className="TodoCounter">
+      Has completado <span>{completed}</span> de <span>{total}</span> TODOs
+    </h1>
+  );
 }
 
 export { TodoCounter };
