3a4999c9055542c14eec8f268c94e14958ccc6a7
Carlos A. Palacios <carlosargelio0104@gmail.com>
2023-11-21 15:45:16 -0400

    fat: used HOC

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import { ToDo as App } from './pages'
+// import './index.css';
+// import { ToDo as App } from './pages'
+
+function App(props) {
+    return (
+      <h1>¡{props.saludo}, {props.nombre}!</h1>
+    );
+  }
+  
+  function withSaludo(saludo) {
+    return function WrappedComponentWithSaludo(WrappedComponent) {
+      return function ComponenteDeVerdad(props) {
+        return (
+          <React.Fragment>
+            <WrappedComponent {...props} saludo={saludo} />
+            <p>Estamos acompañando al WrappedComponent</p>
+          </React.Fragment>
+        );
+      }
+    }
+  }
+  
+  const AppWithSaludo = withSaludo('Wenas')(App);
+  
+//   ReactDOM.render(
+//     <AppWithSaludo nombre="Juanita" />,
+//     // <App saludo="Buenas" nombre="Nath" />,
+//     document.getElementById('root')
+//   );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<AppWithSaludo nombre="Juanita" />);
